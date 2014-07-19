c03908e8e7e807adb184a5ca813a42468be3762a
kyoko igawa <igawakk@gmail.com>
2014-07-20 00:53:18 +0900

    メニュー追加

--- app/src/main/java/berry/yummy/ResultActivity.java
+++ app/src/main/java/berry/yummy/ResultActivity.java
@@ -1,13 +1,21 @@
 package berry.yummy;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends Activity {
 
@@ -52,6 +60,69 @@ public class ResultActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        // メニュー記録ボタン
+        Button addButton = (Button) findViewById(R.id.result_add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ResultActivity.this);
+                builder.setTitle(R.string.result_add_menu_title);
+
+                final EditText menuName = new EditText(ResultActivity.this);
+                final EditText memo = new EditText(ResultActivity.this);
+                TextView menuNameTitle = new TextView(ResultActivity.this);
+                menuNameTitle.setText(R.string.menu_text);
+                menuNameTitle.setTextSize(20);
+                TextView memoTitle = new TextView(ResultActivity.this);
+                memoTitle.setText(R.string.memo_text);
+                memoTitle.setTextSize(20);
+
+                //外枠とパーツの作成
+                LinearLayout layout = new LinearLayout(ResultActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                //外枠にパーツを組み込む
+                layout.addView(menuNameTitle, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layout.addView(menuName, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layout.addView(memoTitle, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layout.addView(memo, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                //レイアウトをダイアログに設定
+                builder.setView(layout);
+
+                // ボタンの追加
+                builder.setPositiveButton(R.string.add_menu_ok_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputMenu = menuName.getText().toString();
+                        String inputMemo = memo.getText().toString();
+                        // メニューの登録
+                        insertMenu(ResultActivity.this.getEatingId(), inputMenu, inputMemo);
+                        Toast.makeText(getApplicationContext(), inputMenu + "を記録しました。", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton(R.string.add_menu_cancel_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    public long insertMenu(long targetEatingId, String menu, String memo) {
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.MENU_NAME, menu);
+        values.put(DatabaseHelper.MENU_MEMO, memo);
+        values.put(DatabaseHelper.MENU_EATING_ID, targetEatingId);
+        long result = -1;
+        if (sqLiteDatabase != null) {
+            result = sqLiteDatabase.insert(DatabaseHelper.MENU_TABLE, null, values);
+        }
+        return result;
     }
 
     @Override
@@ -71,5 +142,9 @@ public class ResultActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public long getEatingId() {
+        return this.eatingId;
     }
 }

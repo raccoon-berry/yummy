60a485982996047bf11f44fae2722f794e804cf9
nakamura <azurecrayon2014@gmail.com>
2014-07-20 13:15:16 +0900

    add progress bar

--- app/src/main/java/berry/yummy/ProgressView.java
+++ app/src/main/java/berry/yummy/ProgressView.java
@@ -11,18 +11,22 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-//public class ProgressView extends View {
 public class ProgressView extends View {
 
     private Bitmap prgImage1 = null;
     private Bitmap prgImage2 = null;
 
-    private int left = 300;
-    private int top = 600;
-    private int incrementPoint = 100;
+    private int left = 230;
+    private int top = 500;
+    private int incrementPoint = 250;
     private int maxTop = 30;
     private String baseColor = "#f0e68c";
-    private int imagePath = R.drawable.smile_girl_small;
+    private int imagePath = R.drawable.smile_girl;
+
+    private float prgBarStart = 700;
+    private float prgBarEnd = 30;
+    private float prgBarLeft = 370;
+    private String prgBarColor = "#228b22";
 
     public ProgressView(Context context){
         super(context);
@@ -52,15 +56,18 @@ public class ProgressView extends View {
         c.drawColor(Color.parseColor(baseColor));
 
         //進捗バー
-
+        Paint pLine = new Paint();
+        pLine.setStrokeWidth(30);
+        pLine.setColor(Color.parseColor(prgBarColor));
+        c.drawLine(prgBarLeft, prgBarStart, prgBarLeft, prgBarEnd, pLine);
 
         //進捗画像
         Paint p = new Paint();
         if(prgImage1 != null) {
             if (top > maxTop) {
                 c.drawBitmap(prgImage1, left, top, p);
-//inValidateのfor文が効かないときの代替手段
-//                top = top - 1;
+                //inValidateのfor文が効かないときので代替手段
+                //top = top - 1;
                 top = top - incrementPoint;
             } else {
                 c.drawBitmap(prgImage1, left, maxTop, p);
